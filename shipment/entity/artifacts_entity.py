0ef11d59046f1eabb93111f275dcae4d3a0603a2
karthigeyanns <124854653+karthigeyanns@users.noreply.github.com>
2025-07-01 16:39:08 +0530

    Update artifacts_entity.py

--- shipment/entity/artifacts_entity.py
+++ shipment/entity/artifacts_entity.py
@@ -21,3 +21,7 @@ class DataTransformationArtifact:
     transformed_train_file_path: str
     transformed_test_file_path: str
     transformed_object_file_path: str
+
+@dataclass
+class ModelTrainerArtifacts:
+    trained_model_file_path: str
